c867e5503d629ac1ae9e24f75836987d0e6874b0
mari4857 <mar4857@gmail.com>
2021-02-21 12:03:51 -0500

    adding the caesar function and writing tests for the caesar function

--- src/caesar.js
+++ src/caesar.js
@@ -5,9 +5,43 @@
 
 const caesarModule = (function () {
   // you can add any code you want within this function scope
-
+  const alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
   function caesar(input, shift, encode = true) {
-    // your solution code here
+    if (!shift || shift < -25 || shift > 25) {
+      return false;
+    }
+    //loop through the input string and create an array containing each letter
+    const inputLowerCase = input.toLowerCase();
+    const inputArray = [];
+    for (let i = 0; i < inputLowerCase.length; i++) {
+      inputArray.push(inputLowerCase[i]);
+    }
+    // for each letter in the input array, if the alphabet array includes it: 
+    // loop through the alphabet array and if the input letter matches the alphabet letter, get index of alphabet letter, 
+    // add or subtract shift from index, push alphabet[shiftedIndex] to an encoded word array
+    // else add the nonalphabetic symbol to the encoded word array
+    let encodedWordArray = [];
+    inputArray.forEach(inputLetter => {
+      if (alphabet.includes(inputLetter)) {
+        alphabet.forEach(alphabetLetter => {
+          if (inputLetter === alphabetLetter) {
+            let shiftedLetterIndex;
+            encode ? shiftedLetterIndex = alphabet.indexOf(alphabetLetter) + shift : shiftedLetterIndex = alphabet.indexOf(alphabetLetter) - shift;
+            if (shiftedLetterIndex >= 26) {
+              shiftedLetterIndex = shiftedLetterIndex - 26;
+            } else if (shiftedLetterIndex < 0) {
+              shiftedLetterIndex = shiftedLetterIndex + 26;
+            }
+            encodedWordArray.push(alphabet[shiftedLetterIndex]);
+          }
+        })
+      } else {
+        encodedWordArray.push(inputLetter);
+      }
+      
+    })
+    // return the joined encoded word array
+    return encodedWordArray.join("");
   }
 
   return {
--- test/caesar.test.js
+++ test/caesar.test.js
@@ -1,1 +1,137 @@
-// Write your tests here!
+const { expect } = require("chai");
+const caesar = require("../src/caesar");
+
+// It returns false if the shift value is equal to 0, less than -25, greater than 25, or not present.
+describe("caesar", () => {
+  it("returns false if the shift value is equal to 0", () => {
+    const input = "thinkful";
+    const shift = 0;
+    const actual = caesar(input, shift, encode = true);
+    const expected = false;
+    expect(actual).to.equal(expected);
+  })
+})
+
+describe("caesar", () => {
+  it("returns false if the shift value is less than -25", () => {
+    const input = "thinkful";
+    const shift = -28;
+    const actual = caesar(input, shift, encode = true);
+    const expected = false;
+    expect(actual).to.equal(expected);
+  })
+})
+
+describe("caesar", () => {
+  it("returns false if the shift value is greater than 25", () => {
+    const input = "thinkful";
+    const shift = 28;
+    const actual = caesar(input, shift, encode = true);
+    const expected = false;
+    expect(actual).to.equal(expected);
+  })
+})
+
+describe("caesar", () => {
+  it("returns false if the shift value is not present", () => {
+    const input = "thinkful";
+    const shift = null;
+    const actual = caesar(input, shift, encode = true);
+    const expected = false;
+    expect(actual).to.equal(expected);
+  })
+})
+
+// It encodes and decodes with positive number shifts and negative number shifts
+describe("caesar", () => {
+  it("encodes with positive number shifts", () => {
+    const input = "thinkful";
+    const shift = 3;
+    const actual = caesar(input, shift, encode = true);
+    const expected = "wklqnixo";
+    expect(actual).to.equal(expected);
+  })
+})
+
+describe("caesar", () => {
+  it("encodes with negative number shifts", () => {
+    const input = "thinkful";
+    const shift = -3;
+    const actual = caesar(input, shift, encode = true);
+    const expected = "qefkhcri";
+    expect(actual).to.equal(expected);
+  })
+})
+
+describe("caesar", () => {
+  it("decodes with positive number shifts", () => {
+    const input = "wklqnixo";
+    const shift = 3;
+    const actual = caesar(input, shift, encode = false);
+    const expected = "thinkful";
+    expect(actual).to.equal(expected);
+  })
+})
+
+describe("caesar", () => {
+  it("decodes with negative number shifts", () => {
+    const input = "qefkhcri";
+    const shift = -3;
+    const actual = caesar(input, shift, encode = false);
+    const expected = "thinkful";
+    expect(actual).to.equal(expected);
+  })
+})
+
+// It ignores capital letters
+describe("caesar", () => {
+  it("ignores capital letters when encoding", () => {
+    const input = "This is a secret message";
+    const shift = 8;
+    const actual = caesar(input, shift, encode = true);
+    const expected = "bpqa qa i amkzmb umaaiom";
+    expect(actual).to.equal(expected);
+  })
+})
+
+describe("caesar", () => {
+  it("ignores capital letters when decoding", () => {
+    const input = "BPQA qa I amkzmb umaaiom";
+    const shift = 8;
+    const actual = caesar(input, shift, encode = false);
+    const expected = "this is a secret message";
+    expect(actual).to.equal(expected);
+  })
+})
+
+// When encoding, it handles shifts that go past the end of the alphabet.
+describe("caesar", () => {
+  it("handles shifts that go past the end of the alphabet", () => {
+    const input = "Zebra";
+    const shift = 3;
+    const actual = caesar(input, shift, encode = true);
+    const expected = "cheud";
+    expect(actual).to.equal(expected);
+  })
+})
+
+// It maintains spaces and other nonalphabetic symbols in the message, before and after encoding or decoding.
+describe("caesar", () => {
+  it("maintains spaces and other nonalphabetic symbols in the message when encoding", () => {
+    const input = "secret message!";
+    const shift = 8;
+    const actual = caesar(input, shift, encode = true);
+    const expected = "amkzmb umaaiom!";
+    expect(actual).to.equal(expected);
+  })
+})
+
+describe("caesar", () => {
+  it("maintains spaces and other nonalphabetic symbols in the message when decoding", () => {
+    const input = "amkzmb umaaiom!";
+    const shift = 8;
+    const actual = caesar(input, shift, encode = false);
+    const expected = "secret message!";
+    expect(actual).to.equal(expected);
+  })
+})
