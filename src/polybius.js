826452e9cddbdc82924524af3490c1d26d8eb100
mari4857 <mar4857@gmail.com>
2021-02-22 20:54:40 -0500

    starting polybius function and tests

--- src/polybius.js
+++ src/polybius.js
@@ -5,9 +5,135 @@
 
 const polybiusModule = (function () {
   // you can add any code you want within this function scope
+  const polybiusCypher = [
+    {
+      letter: "a",
+      number: "11",
+    },
+    {
+      letter: "b",
+      number: "21",
+    },
+    {
+      letter: "c",
+      number: "31",
+    },
+    {
+      letter: "d",
+      number: "41",
+    },
+    {
+      letter: "e",
+      number: "51",
+    },
+    {
+      letter: "f",
+      number: "12",
+    },
+    {
+      letter: "g",
+      number: "22",
+    },
+    {
+      letter: "h",
+      number: "32",
+    },
+    {
+      letter: "i",
+      number: "42",
+    },
+    {
+      letter: "j",
+      number: "42",
+    },
+    {
+      letter: "k",
+      number: "52",
+    },
+    {
+      letter: "l",
+      number: "13",
+    },
+    {
+      letter: "m",
+      number: "23",
+    },
+    {
+      letter: "n",
+      number: "33",
+    },
+    {
+      letter: "o",
+      number: "43",
+    },
+    {
+      letter: "p",
+      number: "53",
+    },
+    {
+      letter: "q",
+      number: "14",
+    },
+    {
+      letter: "r",
+      number: "24",
+    },
+    {
+      letter: "s",
+      number: "34",
+    },
+    {
+      letter: "t",
+      number: "44",
+    },
+    {
+      letter: "u",
+      number: "54",
+    },
+    {
+      letter: "v",
+      number: "15",
+    },
+    {
+      letter: "w",
+      number: "25",
+    },
+    {
+      letter: "x",
+      number: "35",
+    },
+    {
+      letter: "y",
+      number: "45",
+    },
+    {
+      letter: "z",
+      number: "55",
+    }
+  ];
 
   function polybius(input, encode = true) {
-    // your solution code here
+    // if the input contains a number, decode the input, otherwise encode it
+    if (/\d/.test(input)) {
+      console.log("decode input");
+    } else {
+      const encodedInput = [];
+      // loop through input
+      const inputLowerCase = input.toLowerCase();
+      for (let i = 0; i < inputLowerCase.length; i++) {
+        // for each input letter, loop through polybiusCypher
+        const inputLetter = input[i];
+        polybiusCypher.forEach(obj => {
+          // if input letter matches polybiusCypher obj.letter, add obj.number to an array
+          if (inputLetter === obj.letter) {
+            encodedInput.push(obj.number);
+          } else if (inputLetter === " ") {
+            encodedInput.push(inputLetter);
+          }
+        })
+      }
+      return encodedInput.join("");
+    }
   }
 
   return {
--- test/polybius.test.js
+++ test/polybius.test.js
@@ -1,1 +1,68 @@
-// Write your tests here!
+const { expect } = require("chai");
+const polybius = require("../src/polybius");
+
+// spaces are maintained when encoding/decoding
+describe("polybius", () => {
+  it("maintains spaces when encoding", () => {
+    const input = "Hello world";
+    const actual = polybius(input, encode = true);
+    const expected = "3251131343 2543241341";
+    expect(actual).to.equal(expected);
+  })
+})
+
+describe("polybius", () => {
+  it("maintains spaces when decoding", () => {
+    const input = "3251131343 2543241341";
+    const actual = polybius(input, encode = false);
+    const expected = "hello world";
+    expect(actual).to.equal(expected);
+  })
+})
+
+// When encoding/decoding, output should still be a string.
+describe("polybius", () => {
+  it("returns a string when encoding", () => {
+    const input = "thinkful";
+    const actual = polybius(input, encode = true);
+    expect(actual).to.be.a('string');
+  })
+})
+
+describe("polybius", () => {
+  it("returns a string when decoding", () => {
+    const input = "4432423352125413";
+    const actual = polybius(input, encode = false);
+    expect(actual).to.be.a('string');
+  })
+})
+
+// When decoding, the number of characters in the string excluding spaces should be even. Otherwise, return false.
+describe("polybius", () => {
+  it("returns false if number of characters in string (excluding spaces) is not even when decoding", () => {
+    const input = "3251131343 25432413411";
+    const actual = polybius(input, encode = false);
+    const expected = false;
+    expect(actual).to.equal(expected);
+  })
+})
+
+// Capital letters can be ignored.
+describe("polybius", () => {
+  it("ignores capital letters", () => {
+    const input = "Hello";
+    const actual = polybius(input, encode = true);
+    const expected = "3251131343";
+    expect(actual).to.equal(expected);
+  })
+})
+
+// The letters "I" and "J" share a space. When encoding, both letters can be converted to 42, but when decoding, both letters should be shown
+describe("polybius", () => {
+  it("shows the letters 'i' and 'j' when decoding", () => {
+    const input = "4432423352125413";
+    const actual = polybius(input, encode = false);
+    const expected = "th(i/j)nkful";
+    expect(actual).to.equal(expected);
+  })
+})
